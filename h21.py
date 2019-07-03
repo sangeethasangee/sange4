07cc65248f7ed57ab993ccb4c11149d3db54ddc2
sangeethasangee <47592800+sangeethasangee@users.noreply.github.com>
2019-07-03 16:20:54 +0530

    Create h21.py

---
+++ h21.py
@@ -0,0 +1,9 @@
+pq1=int(input())
+rs1=list(map(int,input().split()))
+avg=int(pq1/2)
+la1=rs1[:avg]
+lb1=rs[avg::]
+if ((sum(la)//len(la))==(sum(lb)//len(lb))):
+    print("yes")
+else:
+    print("no")
