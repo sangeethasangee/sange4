ac742e5ada9058aadbfb2d2f8841e44cfecf7656
sangeethasangee <47592800+sangeethasangee@users.noreply.github.com>
2019-07-03 15:33:34 +0530

    Create h13.py

---
+++ h13.py
@@ -0,0 +1,8 @@
+a1,b1=list(map(int,input().split()))
+c1=list(map(int,input().split()))[:a1]
+d1=list(map(int,input().split()))[:b1]
+e1=list(map(int,input().split()))[:b1]
+f1=list(map(int,input().split()))[:b1]
+print(min(c1+d1))
+print(min(c1+e1))
+print(min(c1+f1))
