a8632813a00b53d28486b96d665a1a630d3da23c
sangeethasangee <47592800+sangeethasangee@users.noreply.github.com>
2019-07-09 15:39:39 +0530

    Create p37.py

---
+++ p37.py
@@ -0,0 +1,9 @@
+ps=int(input())
+mn=[int(i) for i in input().split()]
+d=0
+for i in range(1,ps-1):
+    if mn[i]<mn[i-1] and mn[i]<mn[i+1]:
+        d+=1
+    elif mn[i]>mn[i-1] and mn[i]>mn[i+1]:
+        d+=1
+print(d)
