1ed3b60631ac8b716005be3be8548279771c4d30
sangeethasangee <47592800+sangeethasangee@users.noreply.github.com>
2019-07-09 15:30:35 +0530

    Update p30.py

--- p30.py
+++ p30.py
@@ -1,11 +1,11 @@
 ps=input()
-ab=0
+ac=0
 for i in range(0,len(ps)):
     sv=ps[0:i]+hh[i+1::]
     if int(sv)%8==0:
-        ab=1
+        ac=1
         break
-if ab==1:
+if ac==1:
     print("yes")
 else:
     print("no")
